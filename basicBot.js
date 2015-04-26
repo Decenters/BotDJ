d4df124c2456bed9e11940a34d3c1fb1410480a8
Decenters <hfs77120@icloud.com>
2015-04-26 16:31:59 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2146,6 +2146,21 @@
                     }
                 }
             },
+            
+            twitCommand: {
+                command: 'twit',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        if (typeof basicBot.settings.twitLink === "string")
+                            API.sendChat(subChat(basicBot.chat.twitter, {link: basicBot.settings.twitLink}));
+                    }
+                }
+            },
+
 
             jointimeCommand: {
                 command: 'jointime',
